dfc86af2b39593e0d3ae8c8e2938ea079b66ebd6
VulpeOlesea <leseavulpe@gmail.com>
2024-01-10 13:26:43 +0000

    add ex 4 e 5

---
+++ Ex_EstruturaSequencial/5_conv_metros.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite o valor em metros: "))
+centimetros = metros * 100
+
+print(f"Os {metros} metros é {centimetros} centimetros")
+
