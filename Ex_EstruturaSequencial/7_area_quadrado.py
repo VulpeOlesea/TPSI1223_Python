1e805fa75e31f41c76a1db2936d791c156f9f9b5
VulpeOlesea <leseavulpe@gmail.com>
2024-01-12 11:54:02 +0000

    fix ex 7

--- Ex_EstruturaSequencial/7_area_quadrado.py
+++ Ex_EstruturaSequencial/7_area_quadrado.py
@@ -1,10 +1,7 @@
 #Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
-import math
-
 a = float(input("Insira o comprimento do lado do quadrado: "))
-def area_quadrado(a: float):
-    return a*a
+area_quadrado = a ** 2
 
-print(f"A área do quadrado é: {area_quadrado(a)}")
-print(f"O dobro da área do quadrado é: {area_quadrado(a)*2}")
+print(f"A área do quadrado é: {area_quadrado}")
+print(f"O dobro da área do quadrado é: {area_quadrado * 2}")
