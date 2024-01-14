b1aaab2f35a1b9cd9951ad63fdbbce175095df94
VulpeOlesea <leseavulpe@gmail.com>
2024-01-14 00:37:21 +0000

    modif

--- Ex_EstruturaSequencial/16_tinta.py
+++ Ex_EstruturaSequencial/16_tinta.py
@@ -3,10 +3,10 @@
 # que custam € 80,00.
 # Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
-area_pintada = float(input("Insira a área em metros quadrados necessaria para pintar: "))
+area_a_pintar = float(input("Insira a área em metros quadrados necessaria para pintar: "))
 
-lata_tinta = (area_pintada - 1) // (18 * 3) + 1
+lata_tinta = (area_a_pintar - 1) // (18 * 3) + 1
 preco_tinta = lata_tinta * 80
 
-print(f"Para {area_pintada} metros quadrados é necessaria {lata_tinta} de latas de tinta.")
+print(f"Para {area_a_pintar} metros quadrados é necessaria {lata_tinta} de latas de tinta.")
 print(f"Preço total: {preco_tinta} €.")
