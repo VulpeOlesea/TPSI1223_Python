808ab36d6c43469d8a98f7aad0b25ada6e3ff53b
VulpeOlesea <leseavulpe@gmail.com>
2024-01-16 19:00:19 +0000

    add ex 5-7

---
+++ Ex_EstruturaDeDecisao/6_maior_num.py
@@ -0,0 +1,9 @@
+# Faça um Programa que leia três números e mostre o maior deles.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+maior_numero = max(num1, num2, num3)
+
+print(f"O maior número é: {maior_numero}")
