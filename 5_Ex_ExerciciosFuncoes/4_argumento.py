91477da71927ab8b7c42423c64d3c9aaef8ce9fe
VulpeOlesea <leseavulpe@gmail.com>
2024-02-02 21:54:16 +0000

    rename

---
+++ 5_Ex_ExerciciosFuncoes/4_argumento.py
@@ -0,0 +1,9 @@
+# Faça um programa, com uma função que necessite de um argumento.
+# A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
+
+def argumento(a):
+    if a > 0:
+        print("O seu argumento é positivo: P")
+    else:
+        print("O seu argumento é negativo: N")
+
