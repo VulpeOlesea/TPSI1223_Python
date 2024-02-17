8690f3c0a9af1c01aeba88853175d84f635e4496
VulpeOlesea <leseavulpe@gmail.com>
2024-02-17 17:23:31 +0000

    update

--- 2_Ex_EstruturaDeDecisao/04_letra2.py
+++ 2_Ex_EstruturaDeDecisao/04_letra2.py
@@ -4,7 +4,7 @@ letra = str(input("Digite uma letra: ")).upper().strip()
 
 if letra in "A, E, I, O, U":
     print(f"A letra ´{letra}´ é vogal.")
-elif letra in "B, C, D, F, G, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z":
+elif letra in "B, C, D, F, G, J, K, H, L, M, N, P, Q, R, S, T, V, W, Y, X, Z":
     print(f"A letra ´{letra}´ é consoante.")
 else:
     print(f"A ´{letra}´ não é letra.")
